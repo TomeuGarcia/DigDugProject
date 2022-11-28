b4cacd5103c72a587777609988f7853253d40d1a
Aniol <aniolbarnadat@gmail.com>
2022-11-28 10:47:35 +0100

    started on fygar

---
+++ DigDug/js/fygarPrefab.js
@@ -0,0 +1,20 @@
+
+class fygarPrefab extends enemyBase 
+{
+    constructor(_scene, _positionX, _positionY)
+    {
+        super(_scene, _positionX, _positionY, 'fygar', 'fygarInflate', 'fygarWalking', 'fygarGhosting');
+
+
+    }
+
+    preUpdate(time,delta)
+    {
+        super.preUpdate(time, delta);
+    }
+
+    doAttack() // override super's attack
+    {
+        
+    }
+}
